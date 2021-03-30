532a5d42aa3408d7f9b3954ec56f484844cdef7a
lewis <kemboilewis@gmail.com>
2021-03-30 18:42:10 +0300

    added User_account to INSTALLED_APPS and used a custom user model

--- src/rentalink/settings.py
+++ src/rentalink/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #custom apps
+    'User_account',
 ]
+AUTH_USER_MODEL = 'User_account.myUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
