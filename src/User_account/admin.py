c86abc58107e374551277d837e00894fa51e5fd0
lewis <kemboilewis@gmail.com>
2021-03-30 18:58:23 +0300

    added CustomUserAdmin

--- src/User_account/admin.py
+++ src/User_account/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import myUserCreationForm, MyUserChangeForm
+from .models import myUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = myUserCreationForm
+    form = MyUserChangeForm
+    model = myUser
+    list_display = ['username', 'first_name', 'last_name', 'email', 'is_tenant', 'is_landlord','id_number',
+    'Phone_number','profile_image','is_staff', 'is_active','id_copy']
+    #list_display = ['email', 'username',]
+    fieldsets = UserAdmin .fieldsets + (
+        (None, {'fields':('is_tenant','is_landlord','id_number','Phone_number','profile_image','id_copy')}),
+    )#this allow the admin to change this pages
+
+admin.site.register(myUser, CustomUserAdmin)
