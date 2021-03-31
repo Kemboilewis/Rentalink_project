c5e66dd03b4984821364b05d81fe632fde3fd4ce
lewis <kemboilewis@gmail.com>
2021-03-31 12:36:22 +0300

    created the views for index, home, register, login and logout

--- src/User_account/views.py
+++ src/User_account/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages as django_messages
+from .models import *
 
-# Create your views here.
+def index(request):
+    return render(request, 'User_account/index.html')
+
+def landlord_home(request):
+    return render(request, 'User_account/landlord_home.html')
+
+def tenant_home(request):
+    return render(request, 'User_account/tenant_home.html')
+
+#registration and login templates
+def tenant_register(request):
+    return render(request, 'user_account/tenant_register.html')
+
+def landlord_register(request):
+    return render(request, 'user_account/landlord_register.html')
+
+def login_template(request):
+    return render(request, 'user_account/login.html')
+
+#login, logout and registration views
+def login_user(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request,user)
+            if user.is_landlord == True:
+                return redirect('User_account:landlord_home')
+            else:
+                return redirect('User_account:tenant_home')
+        #check if the user has correct credentials
+            django_messages.warning(request, 'INVALID CREDENTIALS!!!')
+            return render(request, 'User_account/login.html')
+
+@login_required()
+def logout_user(request):
+	logout(request)
+	return redirect('User_account:login')
+
+def register_tenant(request):
+	if request.method == 'POST':
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		id_number = request.POST['id_number']
+		contact = request.POST['contact']
+		id_copy = request.POST.get('id_copy', False)
+		profile_image = request.POST.get('profile_image', False)
+		email = request.POST['email']
+		username = request.POST['user_name']
+		password = request.POST['password']
+		
+		#create user
+		user = myUser(username=username,password=password,first_name=first_name, last_name=last_name, id_number=id_number, Phone_number=contact,profile_image=profile_image,email=email,is_tenant=True)
+		try:
+			user.save()
+			return redirect('User_account:login')
+			
+		except:
+			django_messages.error(request, 'unable to create your account')
+			return redirect('User_account:tenant_register_template')
+	else:
+		return redirect('User_account:tenant_register_template')
+
+
+def register_landlord(request):
+	if request.method == 'POST':
+
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		id_number = request.POST['id_number']
+		contact = request.POST['contact']
+		id_copy = request.POST.get('id_copy', False)
+		profile_image = request.POST.get('profile_image', False)
+		email = request.POST['email']
+		username = request.POST['user_name']
+		password = request.POST['password']
+		
+		#crete user
+		user = myUser(username=username,password=password,first_name=first_name, last_name=last_name, id_number=id_number, Phone_number=contact,profile_image=profile_image,email=email,is_landlord=True)
+		try:
+			user.save()
+			login(request,user)
+			return redirect('User_account:login')
+			
+		except:
+			django_messages.error(request, 'unable to create your account')
+			return redirect('User_account:landlord_register_template')
+	else:
+		return redirect('User_account:landlord_register_template')
