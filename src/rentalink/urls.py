142fd232b58e55bcaf549f5a5b8979730492819d
lewis <kemboilewis@gmail.com>
2021-04-22 10:38:33 +0300

    included nyumba.urls in the path

--- src/rentalink/urls.py
+++ src/rentalink/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns= [
     path('admin/', admin.site.urls),
     path('User_account/', include('User_account.urls')),
+    path('nyumba/', include('nyumba.urls')),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
